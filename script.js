48331e0b1680886485f6abaae5c2ea1057cda6a1
Bahtiyor1 <bakhtierbek2011@gmail.com>
2024-09-16 17:29:36 +0500

    JS

---
+++ script.js
@@ -0,0 +1,13 @@
+let Ism = prompt('Ismingizni kiriting')
+let Familiya = prompt('Familiyangizni kiriting')
+let Yosh = prompt('Yoshingizni kiriting')
+let fan = prompt('Yoqtirgan faningizni yozing')
+let manzil = prompt('Yashaydigan tumaningizni kiriting')
+alert('malumotlaringizni kiritganingiz uchun tashakkur')
+
+console.log(`Ism: ${Ism}`)
+console.log(`Familiya: ${Familiya}`)
+console.log(`Yosh: ${Yosh}`)
+console.log(`Yoqtirgan fan: ${fan}`)
+console.log(`Yashash manzili: ${manzil}`)
+
